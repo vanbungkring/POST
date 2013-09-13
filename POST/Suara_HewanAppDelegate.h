4aaaf90742c05c92b7a93d43cf064c4bd9d3ec63
Arie <arie@wego.com>
2013-09-13 20:18:13 +0700

    Initial Commit

---
+++ POST/Suara_HewanAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Suara_HewanAppDelegate.h
+//  POST
+//
+//  Created by Arie on 9/13/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Suara_HewanAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ POST/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  POST
+//
+//  Created by Arie on 9/13/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Suara_HewanAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([Suara_HewanAppDelegate class]));
+	}
+}
