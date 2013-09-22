f61a714b711386e141991d0d460da185cbf405c9
Arie <arie@wego.com>
2013-09-22 11:06:29 +0700

    add global streamer

---
+++ POST/library/helper/streamer/Streamer.h
@@ -0,0 +1,13 @@
+//
+//  Streamer.h
+//  POST
+//
+//  Created by Arie on 9/22/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Streamer : NSURLConnection
+
+@end
---
+++ POST/library/helper/streamer/Streamer.m
@@ -0,0 +1,13 @@
+//
+//  Streamer.m
+//  POST
+//
+//  Created by Arie on 9/22/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import "Streamer.h"
+
+@implementation Streamer
+
+@end
